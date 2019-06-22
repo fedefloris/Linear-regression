438cec94b409905c5cffba60e49afe8ee7a7e169
Federico Floris <fzfloris@gmail.com>
2019-06-22 20:46:53 +0200

    Start to implement evaluate.py

---
+++ srcs/evaluate.py
@@ -0,0 +1,38 @@
+from dataset import Dataset
+from model import LinearRegression
+
+import matplotlib.pyplot as plt
+import argparse
+import numpy as np
+
+def plot(model, dataset):
+    pass
+
+def evaluate(args):
+    dataset = Dataset(args.dataset)
+    model = LinearRegression(n_weights=1)
+    model.load_parameters(args.model_parameters)
+    print (model)
+    plot(model, dataset)
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Evaluate a linear regression model for car price prediction.'
+    )
+    parser.add_argument(
+        'dataset',
+        help='CSV file containing car features (including the price)'
+    )
+    parser.add_argument(
+        '-model_parameters',
+        default='parameters.npy',
+        help='File that contains the trained parameters of the model'
+    )
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    evaluate(args)
+
+if __name__ == '__main__':
+    main()
