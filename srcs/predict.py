6ff3e74b3d4b8b28f89ff8f520a81f9fbdac6826
Federico Floris <fzfloris@gmail.com>
2019-07-01 10:34:30 +0200

    Add srcs/predict.py

---
+++ srcs/predict.py
@@ -0,0 +1,27 @@
+from model import LinearRegression
+
+import argparse
+
+def predict(args):
+    model = LinearRegression()
+    model.load_parameters(args.model_parameters)
+    km = float(input('Enter car\'s kilometers:'))
+    print ('Predicted price:', model.predict([km]))
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Predict the price of a car with a linear regression model.'
+    )
+    parser.add_argument(
+        '-model_parameters',
+        default='parameters.npy',
+        help='File that contains the trained parameters of the model'
+    )
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    predict(args)
+
+if __name__ == '__main__':
+    main()
