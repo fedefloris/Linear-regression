01a80d4d521dfa1aa940f406b3200dedd7f10fc2
Federico Floris <fzfloris@gmail.com>
2019-07-01 17:15:12 +0200

    Save x_max and x_min as object fields

--- srcs/dataset.py
+++ srcs/dataset.py
@@ -9,8 +9,13 @@ class Dataset:
             preprocess=True
         ):
         self._read_data_from_csv(file, delimiter)
+        self._configure()
         if preprocess:
-            self.x = Dataset.preprocess(self.x)
+            self.x = Dataset.preprocess(
+                self.x,
+                self.x_max,
+                self.x_min
+            )
 
     def _read_data_from_csv(self, csv_file, delimiter):
         self.x, self.y = [], []
@@ -21,13 +26,15 @@ class Dataset:
             for row in reader:
                 self.x.append(list(map(float, row[:-1])))
                 self.y.append([float(row[-1])])
-            self.x = np.array(self.x)
-            self.y = np.array(self.y)
-            self.x_raw = self.x
+
+    def _configure(self):
+        self.x = np.array(self.x)
+        self.y = np.array(self.y)
+        self.x_raw = self.x
+        self.x_max = np.amax(self.x, 0)
+        self.x_min = np.amin(self.x, 0)
 
     @staticmethod
-    def preprocess(inputs):
-        # min-max scaling
-        inputs_min = np.amin(inputs, 0)
-        inputs_max = np.amax(inputs, 0)
-        return (inputs - inputs_min) * (1 / (inputs_min - inputs_max))
+    # min-max scaling
+    def preprocess(inputs, inputs_max, inputs_min):
+        return (inputs - inputs_min) / (inputs_max - inputs_min)
